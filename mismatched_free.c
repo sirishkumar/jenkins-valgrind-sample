933dbb33518e5d317a9e1a07aed155702d22f1bd
mark.earl.waite@gmail.com <mark.earl.waite@gmail.com@4d167c9f-71df-50c4-dff7-29fb35990f02>
2012-08-04 12:35:41 +0000

    Move valgrind failures to named functions in mismatched_free

--- mismatched_free.c
+++ mismatched_free.c
@@ -1,18 +1,38 @@
 /* Example program which allocates memory and fails to free it */
 
 #include <stdlib.h>
+#include <string.h>
 
 #define CONSTANT 32
+
+/* ------------------------------------------------------------------------ */
+
+static void
+illegal_free_from_stack (void)
+{
+  const char *s = "A string on the stack";
+  free ((void *) s);		/* Freeing a string on the stack */
+}
+
+/* ------------------------------------------------------------------------ */
+
+static void
+leak_memory (void)
+{
+  int *i_ptr = malloc (sizeof (int));
+  double *d_ptr = malloc (sizeof (double));
+  char *s = malloc (80);
+  *i_ptr = 1234;
+  *d_ptr = 1.234e-5;
+  strcpy (s, "leak_memory function wrote this");
+}
+
+/* ------------------------------------------------------------------------ */
 
 int
 main (void)
 {
-  int *i_ptr = malloc (sizeof (int));
-  const char *s = "A string on the stack";
-
-  *i_ptr = CONSTANT;
-
-  /* free(i_ptr); */
-  free ((void *) s);		/* Freeing a string on the stack */
+  leak_memory();
+  illegal_free_from_stack();
   return 0;
 }
