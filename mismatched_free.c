d9c71a76332f67b6d5c8ad67298a51d9f2e5ca20
mark.earl.waite@gmail.com <mark.earl.waite@gmail.com@4d167c9f-71df-50c4-dff7-29fb35990f02>
2012-08-04 12:25:27 +0000

    Format with indent defaults, add free of memory allocated on the stack

--- mismatched_free.c
+++ mismatched_free.c
@@ -4,12 +4,15 @@
 
 #define CONSTANT 32
 
-int main(void)
+int
+main (void)
 {
-  int *i_ptr = malloc(sizeof(int));
+  int *i_ptr = malloc (sizeof (int));
+  const char *s = "A string on the stack";
 
   *i_ptr = CONSTANT;
 
   /* free(i_ptr); */
+  free ((void *) s);		/* Freeing a string on the stack */
   return 0;
 }
