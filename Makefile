fff09b01d53469c65ed8efba7203eb5a2416f233
mark.earl.waite@gmail.com <mark.earl.waite@gmail.com@4d167c9f-71df-50c4-dff7-29fb35990f02>
2012-08-04 14:34:28 +0000

    Add an overlapping copy example

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 CFLAGS += -g
 
-PROGRAMS = already_freed_memory mismatched_free uninitialized_memory
+PROGRAMS = already_freed_memory mismatched_free overlapping_copy uninitialized_memory
 
 all: $(PROGRAMS)
 
 already_freed_memory: already_freed_memory.c
 
 mismatched_free: mismatched_free.c
+
+overlapping_copy: overlapping_copy.c
 
 uninitialized_memory: uninitialized_memory.c
 
---
+++ overlapping_copy.c
@@ -0,0 +1,24 @@
+/* Example program which copies overlapping memory regions with memcpy */
+
+#include <stdlib.h>
+#include <string.h>
+
+/* ------------------------------------------------------------------------ */
+
+static void
+overlapping_copy (void)
+{
+  void *a1 = malloc (17);
+  void *a2 = a1 + 7;
+  memset (a1, 0, 17);
+  memcpy (a1, a2, 10);		/* Source and destination regions overlap */
+}
+
+/* ------------------------------------------------------------------------ */
+
+int
+main (void)
+{
+  overlapping_copy ();
+  return 0;
+}
