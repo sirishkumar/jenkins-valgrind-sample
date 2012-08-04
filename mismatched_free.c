e6c88cce6fa01bfee361bcf64c0db78215aba724
mark.earl.waite@gmail.com <mark.earl.waite@gmail.com@4d167c9f-71df-50c4-dff7-29fb35990f02>
2012-08-04 12:14:08 +0000

    Assign value to allocated memory in mismatched_free.c

--- mismatched_free.c
+++ mismatched_free.c
@@ -3,10 +3,14 @@
 #include <assert.h>
 #include <stdlib.h>
 
+#define CONSTANT 32
+
 int main(void)
 {
-  int *i = malloc(sizeof(int));
-  assert(i != 0);
-  /* free(i); */
+  int *i_ptr = malloc(sizeof(int));
+  assert(i_ptr != 0);
+  *i_ptr = CONSTANT;
+  assert(*i_ptr == CONSTANT);
+  /* free(i_ptr); */
   return 0;
 }
