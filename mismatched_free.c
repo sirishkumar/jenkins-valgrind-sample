1b3ba84d96f8602f2253657e2c2c77f8223701fd
mark.earl.waite@gmail.com <mark.earl.waite@gmail.com@4d167c9f-71df-50c4-dff7-29fb35990f02>
2012-08-04 12:18:38 +0000

    Remove assertions from mismatched_free.c

--- mismatched_free.c
+++ mismatched_free.c
@@ -1,6 +1,5 @@
 /* Example program which allocates memory and fails to free it */
 
-#include <assert.h>
 #include <stdlib.h>
 
 #define CONSTANT 32
@@ -8,9 +7,9 @@
 int main(void)
 {
   int *i_ptr = malloc(sizeof(int));
-  assert(i_ptr != 0);
+
   *i_ptr = CONSTANT;
-  assert(*i_ptr == CONSTANT);
+
   /* free(i_ptr); */
   return 0;
 }
